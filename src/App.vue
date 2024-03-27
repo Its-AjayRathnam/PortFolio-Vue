9c4957ea64c06b2f463fb22d5160cadc53514553
Ajay Rathnam <its.ajayrathnam@gmail.com>
2024-03-27 22:28:04 +0530

    New files

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <v-layout class="rounded rounded-md bg-white">
+    <v-navigation-drawer class="bg-white">
+      <v-list>
+        <v-list-item title="Navigation items"></v-list-item>
+      </v-list>
+      </v-navigation-drawer>
+    <v-app-bar title="Ajay Rathnam" class="bg-white font-bold"
+    color="white">
+  <div id="navv" class="float-left"> 
+   
+    <v-btn class="rounded-circle d-inline-block" @click="toggleTheme"
+    v-if="brightness"><tabler.IconSun class="rounded-circle"/></v-btn>
+    <v-btn class="rounded-circle d-inline-block" @click="toggleTheme" v-else ><v-icon><tabler.IconMoon/></v-icon></v-btn>
+  </div>
+    </v-app-bar>
+    
+    <v-main class="d-flex justify-center align-center bg-amber">
+      
+  <router-view></router-view>
+    </v-main>
+  </v-layout>
+  </template>
+  
+  <script setup>
+  import { useTheme } from 'vuetify'
+  import { tabler } from './components/Tabler/tabler-icons'
+  const theme = useTheme()
+  let brightness = true;
+  function toggleTheme () {
+    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+    brightness = !brightness
+    console.log('brightness', brightness)
+  }
+  </script>
---
+++ src/components/Tabler/tabler-icons.js
@@ -0,0 +1,13 @@
+
+import { IconHome, IconCamera, IconHeart,IconBrightness,IconSun, IconMoon /* ...other icons */ } from '@tabler/icons-vue';
+
+export const tabler = {
+  IconHome,
+  IconCamera,
+  IconHeart,
+  IconBrightness,
+  IconSun,
+  IconMoon
+  // ...add other icons here
+};
+
