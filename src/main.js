35bbbc33f73d977ec6018eed11bc82a209a520da
Ajay Rathnam <its.ajayrathnam@gmail.com>
2024-03-31 14:09:58 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -9,12 +9,30 @@ import { registerPlugins } from '@/plugins'
 
 // Components
 import App from './App.vue'
-
+import Home from '../Components/Home/Home.vue'
+import Skills from '../Components/Skills/Skills.vue'
+import About from '../Components/About/About.vue'
 // Composables
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [{
+    path: '/',
+    component: Home 
+},{
+    path: '/Skills',
+    component: Skills
+},{
+    path: '/About',
+    component: About
+}
+]
+const router = createRouter([{
+    history: createWebHistory(),
+    routes: routes
+}])
 const app = createApp(App)
 
 registerPlugins(app)
-
+app.use(router)
 app.mount('#app')
